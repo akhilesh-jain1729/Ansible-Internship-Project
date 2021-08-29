1d88bd7ff373fe6927b6924b38179f6a46df8315
Akhilesh Jain <64414173+akhilesh-jain1729@users.noreply.github.com>
2021-08-29 22:05:41 +0530

    Create variables.tf

---
+++ jenkins/variables.tf
@@ -0,0 +1,17 @@
+variable "token" {
+  type = string
+  default = "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"
+  description = "Token for authentication"
+}
+
+variable "repo_name" {
+  type = string
+  default = "Ansible-Internship-Project"
+  description = "Repository Name of Github"
+}
+
+variable "repo_url" {
+  type = string
+  default = "https://<user>:<pass>@github.com/akhilesh-jain1729/Ansible-Internship-Project.git"
+  description = "Repository Url"
+}
